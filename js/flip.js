1aa0570c7d2ac80c72d939f6e1876420c7069456
Cathy Lee <cathylee03@gmail.com>
2015-05-14 19:40:41 -0600

    wow fixed a merge conflict from hell

--- js/flip.js
+++ js/flip.js
@@ -1,58 +1,54 @@
-/**
- * Created by cathylee on 5/11/15.
- */
 
 $(document).ready(function() {
 
-  $("#go").click(function(){ getMostViewed(); $('.nytImageContainer').empty(); });
-
-
+  $("#go").click(function(){
+      getMostViewed();
+      $('.nytImageContainer').empty();
+  });
 
 });
 
 
 function getMostViewed() {
   var keyword = $('#nytInput').val().split(' ').join('+');
+
   console.log(keyword);
 
-  var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?callback=svc_search_v2_articlesearch&q=" + encodeURIComponent(keyword) + "&sort=newest&page=1&api-key=18a905653b4f0581867fa5199b5156e0%3A5%3A72064799";
+  var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?callback=svc_search_v2_articlesearch&q="
+      + encodeURIComponent(keyword)
+      + "&sort=newest&page=1&api-key=18a905653b4f0581867fa5199b5156e0%3A5%3A72064799";
+
   console.log(url);
 
 
   $.ajax({
     url: url,
     context: document.body
-  })
-  .done(function(data){
+  }).done(function(data){
     // console.log(data);
 
     var articles = data.response.docs;
     console.log(articles);
-    articles.forEach(function(e){
 
     articles.forEach(function(e, i){
       var headline = e.headline.print_headline;
       console.log (headline);
       if( headline !== undefined ) {
-      $('.nytImageContainer').append("<div class='row headline'><h4>" + headline + "</h4>");
-      } else
-      $('.nytImageContainer').append("<div class='row headline'><h4 style='color: #DDD;'>'no title provided by nyt'</h4>"); 
+        $('.nytImageContainer').append("<div class='row headline'><h4>" + headline + "</h4>");
+      } else {
+        $('.nytImageContainer').append("<div class='row headline'><h4 style='color: #DDD;'>'no title provided by nyt'</h4>");
+      }
 
-
-      var website = e.web_url;
-      console.log (website);
-      $('.nytImageContainer').append("<div class='row headline'><p><a href='" + website + "'>" + website + "</a></p>");
-
+        var website = e.web_url;
+        console.log (website);
+        $('.nytImageContainer').append("<div class='row headline'><p><a href='" + website + "'>" + website + "</a></p>");
 
       var multimedia = e.multimedia[1];
       // console.log(multimedia);
 
       if (!jQuery.isEmptyObject(multimedia)) {
-          // console.log(multimedia);
-          // multimedia.forEach(function(e){
             var imgUrl = multimedia.url
-            // console.log(imgUrl);
-            // return (imgUrl);
+
             $('.nytImageContainer').append("<div class='col-xs-12 nytImgDiv'><img src='http://www.nytimes.com/" + imgUrl + "' style='border-radius: 10px; margin: 20px;'/></div>");
 
         }
@@ -62,25 +58,5 @@ function getMostViewed() {
       }
     });
 
-    // articles.forEach(function(e){
-    //   var articleUrl = e.web_url;
-    //   console.log(articleUrl);
-    // });
-
-    // articles.forEach(function(e){
-    //   var multimedia = e.multimedia;
-    //   console.log(multimedia);
-    //   if (!jQuery.isEmptyObject(multimedia)) {
-    //       // console.log(multimedia);
-    //       multimedia.forEach(function(e){
-    //         var imgUrl = e.url
-    //         console.log(imgUrl);
-    //
-    //         $('.articleImage').append("<img src='http://www.nytimes.com/" + imgUrl + "' class='pull-left' style='border-radius: 50px'/>");
-    //     })}
-    //   else { }
-    //   // console.log(imgUrl);
-    // });
-
-  })
+  });
 }
